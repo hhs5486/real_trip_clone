92af2bc96db97b386530014f2cd9fa3799198d9b
hhs5486 <hhs5486@naver.com>
2025-04-26 18:22:12 +0900

    04-26/18:22

--- src/body.jsx
+++ src/body.jsx
@@ -5,6 +5,7 @@ function body() {
     <div className="body">
       <div>
         <h1>국내 숙소</h1>
+
         <div>
           <div className="info">
             <div className="info_div">
@@ -30,22 +31,43 @@ function body() {
             <button className="serch_button">검색</button>
           </div>
         </div>
-        <div className="img_advertisement_div">
-          <img
-            className="img_advertisement"
-            src="src/assets/main/advertisement.jpg"
-          />
-          <div className="number_span_div">
-            <span id="number_span" className="number_span">
-              1/4
-            </span>
+
+        <div className="ad">
+          <div className="img_advertisement_div">
+            <img
+              className="img_advertisement"
+              src="src/assets/main/advertisement.jpg"
+            />
+            <div className="number_span_div">
+              <span id="number_span" className="number_span">
+                1/4
+              </span>
+            </div>
+            <div className="left_button_div">
+              <button>
+                <img src="src/assets/main/free-icon-left-arrow-271220.png" />
+              </button>
+            </div>
+            <div className="right_button_div">
+              <button>
+                <img src="src/assets/main/free-icon-right-arrow-271228.png"></img>
+              </button>
+            </div>
           </div>
-          <button className="left_button">←</button>
-          <button className="right_button">→</button>
+        </div>
+
+        <div>
+          <h4>마이리얼 트립 할인 이벤트</h4>
+          <div>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div> 
+            </div>
+          </div>
         </div>
       </div>
-      <div></div>
-      <div></div>
     </div>
   );
 }
